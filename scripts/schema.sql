4363e66bc2174c4df759188cb350b30fe7854a69
Mohammed Ibrahim <mohammedibrahim@Mohammeds-MacBook-Air.local>
2026-02-03 15:28:31 +0530

    Create DB tables before running tests in CI

---
+++ scripts/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    role TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS products (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    price NUMERIC NOT NULL
+);
